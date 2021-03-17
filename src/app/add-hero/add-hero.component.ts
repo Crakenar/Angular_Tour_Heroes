1a94150291c55d38cf5bfbb67138ecbf347ab808
Teo Berguerre <teo.berguerre@etu.univ-grenoble-alpes.fr>
2021-03-17 11:11:07 +0100

    form hero ok

--- src/app/add-hero/add-hero.component.ts
+++ src/app/add-hero/add-hero.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, SimpleChange, OnChanges, SimpleChanges} from '@angular/core';
 import {HeroService } from '../Services/hero.service';
 import { Hero } from '../data/hero';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-hero',
@@ -8,27 +9,43 @@ import { Hero } from '../data/hero';
   styleUrls: ['./add-hero.component.css']
 })
 export class AddHeroComponent implements OnInit {
+  private static INITIAL_POINTS = 40;
 
-  @Input() name: string | undefined;
-  @Input() pv: number | undefined;
-  @Input() esquive: number | undefined;
-  @Input() degats: number | undefined;
-  @Input() attaque: number | undefined;
-  constructor(private heroService: HeroService) { }
+  heroForm: FormGroup;
+  nbrPointRestant: number;
+  constructor(private heroService: HeroService, public formBuilder: FormBuilder) {
+    this.nbrPointRestant = AddHeroComponent.INITIAL_POINTS;
+    this.heroForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      pv: ['', [Validators.required, Validators.min(1), Validators.max(37)]],
+      attaque: ['', [Validators.required, Validators.min(1), Validators.max(37)]],
+      degats: ['', [Validators.required, Validators.min(1), Validators.max(37)]],
+      esquive: ['', [Validators.required, Validators.min(1),, Validators.max(37)]],
+    });
+  }
 
   ngOnInit(): void {
+    this.nbrPointRestant = AddHeroComponent.INITIAL_POINTS;
+    this.onChangesForm();
   }
 
-  onSubmit(val: any): void {
-    console.log(val);
-    const hero = new Hero();
-    hero.name = val.name;
-    hero.pv = val.pv;
-    hero.attaque = val.attaque;
-    hero.degats = val.degats;
-    hero.esquive = val.esquive;
-    hero.arme = 'Hache';
-    hero.points = 40;
+  // tslint:disable-next-line:typedef
+  get getControl(){
+    return this.heroForm.controls;
+  }
+
+  onChangesForm(): void {
+    this.heroForm.valueChanges.subscribe(value => {
+      // console.log(value);
+      console.log(this.heroForm.controls);
+      this.nbrPointRestant = AddHeroComponent.INITIAL_POINTS;
+      this.nbrPointRestant = this.nbrPointRestant - value.pv - value.attaque - value.esquive - value.degats;
+    });
+  }
+
+  onSubmit(): void {
+    const hero = this.heroForm.value;
     this.heroService.addHero(hero);
   }
+
 }

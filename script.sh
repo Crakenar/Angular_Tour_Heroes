b89fed285e089b7de09bf42b15027a4c4093d0b8
Crakenar <teo.berguerre@etu.univ-grenoble-alpes.fr>
2021-04-01 16:09:12 +0200

    script

--- script.sh
+++ script.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-gitRepository="https://github.com/Crakenar/Angular_Tour_Heroes.git"
-
-localFolder= $(pwd)
-echo $localFolder
-
-git clone "$repository" "$localFolder"
+echo "Make sur there is no folder named Angular_Tour_Heroes "
+echo "If there is, make sure it is empty"
+echo $PWD
+echo "Cloning repertory"
+git clone "https://github.com/Crakenar/Angular_Tour_Heroes.git" $PWD/Angular_Tour_Heroes
+cd "Angular_Tour_Heroes";
+echo "install npm dependencies"
+sudo npm install
+echo "npm audit fix"
+sudo npm audit fix
+echo "ng build prod"
+sudo ng build --prod
+echo "create docker images"
+echo $PWD
+sudo docker build -t angular_tour_heroes_image .
+echo "Maybe your port 81 is already used"
+firefox -P 'Another Profile' http://localhost:81
+echo "Refresh the page"
+sudo docker run -p 81:80 angular_tour_heroes_image
